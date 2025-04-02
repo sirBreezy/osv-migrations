16d349651d19fdbb445cb3dc9f670c3a5aea3486
brizzle <kristafer@gmail.com>
2025-04-02 01:14:16 -0400

    added delete_migration_plan.py

---
+++ delete_migration_plan.py
@@ -0,0 +1,30 @@
+import requests
+
+def delete_migration_plan(cluster_url, namespace, plan, auth_token):
+
+    url = f"{cluster_url}/apis/forklift.konveyor.io/v1beta1/namespaces/{namespace}/plans/{plan}"
+    
+    headers = {
+        "Authorization": f"Bearer {auth_token}",
+        "Accept": "application/json"
+    }
+
+     # Suppress SSL warnings (only use in non-production environments)
+    requests.packages.urllib3.disable_warnings()
+
+    response = requests.delete(url, headers=headers, verify=False) # verify=False can be removed for legitimate SSL Certificates
+
+    if response.status_code in [200, 202, 204]:
+        print(f"Migration plan '{plan}' deleted successfully.")
+        return True
+    else:
+        print(f"Failed to delete plan '{plan}': {response.status_code} - {response.text}")
+        return False
+
+# Example usage
+cluster_url = "https://api.ocp4.example.com:6443"
+namespace = ""
+plan = ""
+auth_token = ""
+ 
+delete_migration_plan(cluster_url , namespace, plan, auth_token)
